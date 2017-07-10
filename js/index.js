91451573abae936d0902a25a18da8b831720efd7
Hugo Verger <hverger@wanadoo.fr>
2017-07-11 01:45:34 +0200

    Design completed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	$('#fullpage').fullpage({
-		sectionsColor: ['#131313', '#181818', '#232323', '#282828'],
-		anchors: ['introPage', 'page2', 'page3', 'contactPage'],
+		sectionsColor: ['#131313', '#171717', '#212121', '#252525', '#292929', '#252525', '#212121', '#171717', '#131313'],
+		anchors: ['IntroPage', 'ImpulsePage', 'NinjaWayPage', 'DeeperPage', 'JeuxGratuitsPage', 'CaptivesPage', 'SwitchPage', '42daysPage', 'ContactPage'],
 		menu: '#menu',
 		navigation: true,
 		navigationPosition: 'right',
-		navigationTooltips: ['First page', 'Second page', 'Third page', 'Contact page'],
+		navigationTooltips: ['Intro', 'Impulse', 'Ninja Way', 'Deeper', 'Jeux-Gratuits.com', 'Captives', 'Switch ON-OFF', '42 Days Later', 'Contact'],
 		loopHorizontal: false,
 		onLeave: function(index, nextIndex, direction){
 			$('#Fullscreen').fadeOut();
@@ -13,13 +13,49 @@ $(document).ready(function(){
 	});
 
 	$('.galleryImage').click(function(){
-		var src = $(this).attr('src'); //get the source attribute of the clicked image
-		$('#Fullscreen').attr('src', src); //assign it to the tag for your fullscreen div
+		var src = $(this).attr('src');
+		$('#Fullscreen').attr('src', src);
 		$('#Fullscreen').fadeIn();
 	});
 
 	$('#Fullscreen').click(function(){
-		$(this).fadeOut(); //this will hide the fullscreen div if you click away from the image.
+		$(this).fadeOut();
+	});
+
+	$('#EnglishButton').click(function(){
+		alert("English !");
+	});
+
+	$('#FrenchButton').click(function(){
+		alert("Français !");
+	});
+
+	$('#ImpulseLink').click(function(){
+		$.fn.fullpage.moveTo(2);
+	});
+
+	$('#NinjaWayLink').click(function(){
+		$.fn.fullpage.moveTo(3);
+	});
+
+	$('#DeeperLink').click(function(){
+		$.fn.fullpage.moveTo(4);
+	});
+
+	$('#JeuxGratuitsLink').click(function(){
+		$.fn.fullpage.moveTo(5);
+	});
+
+	$('#CaptivesLink').click(function(){
+		$.fn.fullpage.moveTo(6);
+	});
+
+	$('#SwitchLink').click(function(){
+		$.fn.fullpage.moveTo(7);
+	});
+
+	$('#42daysLink').click(function(){
+		$.fn.fullpage.moveTo(8);
 	});
 });
 
