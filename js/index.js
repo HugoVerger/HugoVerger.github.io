ae0c94d7307f55e0877d9732674dae3ebb15fbc0
Hugo Verger <hverger@wanadoo.fr>
2017-07-10 21:21:17 +0200

    Video and image gallery presentations templates are done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-//Hello
+$(document).ready(function(){
+	$('#fullpage').fullpage({
+		sectionsColor: ['#131313', '#181818', '#232323', '#282828'],
+		anchors: ['introPage', 'page2', 'page3', 'contactPage'],
+		menu: '#menu',
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['First page', 'Second page', 'Third page', 'Contact page'],
+		loopHorizontal: false,
+		onLeave: function(index, nextIndex, direction){
+			$('#Fullscreen').fadeOut();
+		}
+	});
+
+	$('.galleryImage').click(function(){
+		var src = $(this).attr('src'); //get the source attribute of the clicked image
+		$('#Fullscreen').attr('src', src); //assign it to the tag for your fullscreen div
+		$('#Fullscreen').fadeIn();
+	});
+
+	$('#Fullscreen').click(function(){
+		$(this).fadeOut(); //this will hide the fullscreen div if you click away from the image.
+	});
+});
+
